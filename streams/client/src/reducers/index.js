ae2240da11b348e43dc60b4d3c2f21d4504e3d83
Mehmet <equaco@gmail.com>
2019-05-01 13:28:15 +0300

    Read&Edit&Create done,db api done,Rest implemented,URL based selection implemented

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as FormReducer } from 'redux-form'
 import authReducer from "./authReducer";
+import streamReducer from './streamReducer';
+
 
 export default combineReducers({
     auth: authReducer,
-    form: FormReducer
+    form: FormReducer,
+    streams: streamReducer
 });
